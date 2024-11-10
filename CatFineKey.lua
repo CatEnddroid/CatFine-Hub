0f12641a5ea35660ba9c7fcefbd840c04fbf997e
CatEnddroid <damon.nuber@gmx.ch>
2024-11-10 13:00:33 +0100

    Aktualisieren von CatFineKey.lua

--- CatFineKey.lua
+++ CatFineKey.lua
@@ -1,63 +1,67 @@
+--[[
+  WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
+]]
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-
-local Window = OrionLib:MakeWindow({Name = "Key system", HidePremium = false, SaveConfig = true, IntroText = "Loading..."})
+local Window = OrionLib:MakeWindow({Name = "KeySystem", HidePremium = true, SaveConfig = true, ConfigFolder = "OrionTest"})
 
 OrionLib:MakeNotification({
-	Name = "Get Key",
-	Content = "uhhh hello",
-	Image = "rbxassetid://4483345998",
-	Time = 3
+  Name = "Your in!",
+  Content = "Your In The Special LEAN party",
+  Image = "rbxassetid://4483345998",
+  Time = 5
 })
 
-_G.Key = "Test"
+_G.Key = "NEW" -- You Put Your Key Here
 _G.KeyInput = "string"
 
 function MakeScriptHub()
-    loadstring(game:httpget("https://github.com/CatEnddroid/CatFine-Hub/blob/main/CatFineHub.lua"))()
-	
+loadstring(game:HttpGet(("https://github.com/CatEnddroid/CatFine-Hub/blob/main/CatFineHub.lua"), true))() -- You Put Your Script Here
+
 end
 
-function CorrectKeyNotification()
+
+function CorrectKeyNotifications()
     OrionLib:MakeNotification({
-	Name = "Correct Key!",
-	Content = "you paid yay",
-	Image = "rbxassetid://4483345998",
-	Time = 3
+        Name = "Correct Key",
+        Content = "Welcome To The LEAN PARTY :D",
+        Image = "rbxassetid://4483345998",
+        Time = 5
     })
 end
 
-function IncorrectKeyNotification()
+function WrongKeyNotifications()
     OrionLib:MakeNotification({
-	Name = "Incorrect Key",
-	Content = "Uhhh pay",
-	Image = "rbxassetid://4483345998",
-	Time = 3
+        Name = " Wrong Key",
+        Content = "Your Not on The List Kid",
+        Image = "rbxassetid://4483345998",
+        Time = 5
+    })
 end
 
-                
 local Tab = Window:MakeTab({
-	Name = "Key",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
+  Name = "Key Room",
+  Icon = "rbxassetid://4483345998",
+  PremiumOnly = false
 })
 
 Tab:AddTextbox({
-	Name = "Enter key",
-	Default = "",
-	TextDisappear = true,
-	Callback = function(Value)
-		_G.KeyInput = Value
-	end	  
+  Name = "Enter Key",
+  Default = "Enter Key",
+  TextDisappear = true,
+  Callback = function(Value)
+        _G.KeyInput = Value
+  end	  
 })
 
 Tab:AddButton({
-	Name = "Check Key",
-	Callback = function()
-      	if _G.KeyInput == _G.Key then
-        MakeScriptHub()
-        CorrectKeyNotification()
-        else
-            IncorrectKeyNotification()
+  Name = "Check Key!",
+  Callback = function()
+        if _G.KeyInput == _G.Key then
+         MakeScriptHub()
+         CorrectKeyNotifications()
+     else
+        WrongKeyNotifications()
         end
-  	end    
+    end    
 })
+
